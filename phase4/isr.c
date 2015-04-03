7b9b3d8fd44bcdcb8db98fa81810a5da89080afd
AdamStillman <adam.still@gmail.com>
2015-04-02 18:36:32 -0700

    Update isr.c

--- phase4/isr.c
+++ phase4/isr.c
@@ -152,7 +152,7 @@ outportb(0x20, 0x67);
 
 if(semaphore[print_semaphore].wait_q.size <= 0) semaphore[print_semaphore].count++;
 else{
-	pid = DeQ(&semaphore[print_semaphore].wait_q);
+	pid = DeQ(&(semaphore[print_semaphore].wait_q));
 	EnQ(pid, &run_q);
 	pcb[pid].state = RUN;	
 }
