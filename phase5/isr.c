16942a51f3b7e320e10dad64addf50d502becc53
AdamStillman <adam.still@gmail.com>
2015-04-06 19:51:49 -0700

    Update isr.c

--- phase5/isr.c
+++ phase5/isr.c
@@ -157,7 +157,7 @@ else{
 void MsgSendISR(){
 	int mid;
 	msg_t *local_msg;
-	local_msg = (msg_t*) pcb[CRP].TF_ptr ->ebx;
+	local_msg = (msg_t *) pcb[CRP].TF_ptr ->ebx;
 	
 	if(mbox[CRP].wait_q){
 		
