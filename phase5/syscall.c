4fb6b27957c5cfafb612df540a38f49832255ce9
AdamStillman <adam.still@gmail.com>
2015-04-06 19:06:01 -0700

    Update syscall.c

--- phase5/syscall.c
+++ phase5/syscall.c
@@ -37,5 +37,18 @@ int SemGet(int count) {
 		: "%ebx", "%ecx" ); // push/pop before/after asm()
   return sid;
 }
+//phase 5
+int MsgSend(msg_t *msg) {
+	asm("movl %0, %%ebx; int $53;" // CPU inst
+		:
+		: "g" (int)(msg)
+		: "%ebx"); // push/pop before/after asm()
+}
 
+int MsgRecieve(msg_t *msg) {
+	asm("movl %0, %%ebx; int $54;" // CPU inst
+		:
+		: "g" (int)(msg)
+		: "%ebx"); // push/pop before/after asm()
+}
 
