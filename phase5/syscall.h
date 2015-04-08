da1ffb3e32d169bea26f6bc9825c5bb5e406bfca
AdamStillman <adam.still@gmail.com>
2015-04-07 17:45:41 -0700

    Update syscall.h

--- phase5/syscall.h
+++ phase5/syscall.h
@@ -2,13 +2,14 @@
 
 #ifndef _SYSCALL_H_
 #define _SYSCALL_H_
+#include "type.h"
 
 int GetPid();      // no input, 1 return
 void Sleep(int);   // 1 input, no return
 void SemWait(int);
 void SemPost(int);
 int SemGet(int);
-void MsgSend(msg_t*);
-void MsgRecieve(msg_t*);
+void MsgSend(msg_t *);
+void MsgRecieve(msg_t *);
 
 #endif
