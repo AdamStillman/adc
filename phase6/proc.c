dee101fdee2ab905a9e037a955ec710783a090da
AdamStillman <adam.still@gmail.com>
2015-04-21 17:33:52 -0700

    fixing proc

--- phase6/proc.c
+++ phase6/proc.c
@@ -97,7 +97,7 @@ void shell () {
    MyBzero((char *) &terminal.echo_q,sizeof(q_t));//clear 3 queues: TX_q, RX_q, echo_q
    terminal.TX_sem = SemGet(Q_SIZE);   //get a semaphore to set TX_sem, count Q_SIZE (char space to terminal video)
    terminal.RX_sem = SemGet(0);   //get a semaphore to set RX_sem, count 0 (no char from terminal KB)
-   terminalecho = 1;   //set echo to 1 (default is to echo back whatever typed from terminal)
+   terminal.echo = 1;   //set echo to 1 (default is to echo back whatever typed from terminal)
    terminal.TX_extra = 1;   //set TX_extra to 1 (an IRQ3 TXRDY event missed)
 
 
@@ -136,68 +136,68 @@ void shell () {
      
       // prompt valid commands (send msg to STDOUT, receive reply)
       MyStrCpy(msg.data, "whoami, bye");
-       MsgSnd(STDOUT, &msg);
-       MsgRcv(&msg);
+       MsgSend(STDOUT, &msg);
+       MsgRecieve(&msg);
       
        //  prompt for login (send msg to STDOUT, receive reply)
        MyStrCpy(msg.data, "login: ");
-       MsgSnd(STDOUT, &msg);
-       MsgRcv(&msg);
+       MsgSend(STDOUT, &msg);
+       MsgRecieve(&msg);
        
        //  get login entered (send msg to STDIN, receive reply)
-	MsgSnd(STDIN, &msg);
-	MsgRcv(&msg);
+	MsgSend(STDIN, &msg);
+	MsgRecieve(&msg);
 	MyStrCpy(login, msg.data);
 	
        //  prompt for password (same as above)
        MyStrCpy(msg.data, "password: ");
-       MsgSnd(STDOUT, &msg);
-       MsgRcv(&msg);
+       MsgSend(STDOUT, &msg);
+       MsgRecieve(&msg);
        
        //  get password entered (same as above)
-       MsgSnd(STDIN, &msg);
-       MsgRcv(&msg);
+       MsgSend(STDIN, &msg);
+       MsgRecieve(&msg);
        MyStrCpy(password, msg.data);
        
         
-       if(MyStrcmp(login, password)) break; // string-compare login and password; if same, break loop A
+       if(MyStrCmp(login, password)) break; // string-compare login and password; if same, break loop A
        
        else {  //(else) prompt "Invalid login!\n\0"
        	MyStrCpy(msg.data, "Invalid login! ");
-       	MsgSnd(STDOUT, &msg);
-       	MsgRcv(&msg);
+       	MsgSend(STDOUT, &msg);
+       	MsgRecieve(&msg);
        }
        
      } //end of loop a
      while(1) {//loop B:
      //prompt for entering command string
      MsgStrCpy(msg.data, "enter command: "); 
-     MsgSnd(STDOUT, &msg);
-     MsgRcv(&msg);
+     MsgSend(STDOUT, &msg);
+     MsgRecieve(&msg);
     
      //get command string entered
-     MsgSnd(STDIN, &msg);
-     MsgRcv(&msg);
+     MsgSend(STDIN, &msg);
+     MsgRecieve(&msg);
      //   if command string is empty, then continue (loop B)
       if(MyStrlen(msg.data) == 0) continue;
       else if(MyStrcmp(msg.data, "bye\0")) break; //if command string is "bye", then break (loop B)
       else if (MyStrcmp(msg.data, "whoami\0")) { //if command string is "whoami"
        //show login string,
        MyStrCpy(msg.data,login);
-       MsgSnd(STDOUT, &msg);
-       MsgRcv(&msg);
+       MsgSend(STDOUT, &msg);
+       MsgRecieve(&msg);
        
        //and an additional "\n\0" (for aesthetics)
        MyStrCpy(msg.data, "\n\0");
-       MsgSnd(STDOUT, &msg);
-       MsgRcv(&msg);
+       MsgSend(STDOUT, &msg);
+       MsgRecieve(&msg);
        continue; // continue (loop B)
       } //if command string is "whoami" (end)
       else { //else other strings are entered in command string
    	//show "Command not found!\n\0"
    	MyStrCpy(msg.data, "Command not found!\n\0");
-   	MsgSnd(STDOUT, &msg);
-   	MsgRcv(&msg);
+   	MsgSend(STDOUT, &msg);
+   	MsgRecieve(&msg);
       }
      }// if command string is empty block (end)
    }//  repeat loop B
@@ -206,14 +206,14 @@ void shell () {
 
 
 void STDIN(){
-cha *p, ch;
+ch *p, ch;
 msg_t msg;
 	while(1){
 		MsgRecieve(&msg);
 		*p=msg.data;
 		while(1){
 			SemWait(terminal.RX_sem);
-			ch = (char)DeQ(terminal.RX_q);
+			ch = (char)DeQ(&terminal.RX_q);
 			if(ch=='\r') break;
 			*p++ = ch;
 		}
@@ -232,11 +232,11 @@ msg_t msg;
 		*p = msg.data;
 		while(1){
 			SemWait(terminal.TX_sem);
-			EnQ((int) p, terminal.TX_q);
+			EnQ((int) p, &terminal.TX_q);
 			TipIRQ3();
 			if(*p=='\n'){
 				SemWait(terminal.TX_sem);
-				EnQ('\r', terminal.TX_q);
+				EnQ('\r', &terminal.TX_q);
 			}		
 		p++
 		}
