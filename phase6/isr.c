42d636fa3ddbcf2adbb72effb8b39dcb617969c1
AdamStillman <adam.still@gmail.com>
2015-04-21 19:49:54 -0700

    Update isr.c

--- phase6/isr.c
+++ phase6/isr.c
@@ -33,21 +33,21 @@ void CreateISR(int pid) {
 	//	cons_printf("in idle\n");
 	}
 	else if(pid==1){
-		pcb[pid].TF_ptr->eip = (unsigned int) Init;
+		pcb[pid].TF_ptr->eip = (unsigned int)Init;
 	}
 	else if(pid==2){
-		pcb[pid].TF_ptr->eip = (unsigned int) PrintDriver;
+		pcb[pid].TF_ptr->eip = (unsigned int)PrintDriver;
 	}
 	else if(pid==3){
-		pcb[pid].TF_ptr->eip = (unsigned int) shell;
+		pcb[pid].TF_ptr->eip = (unsigned int)shell;
 	}
 	else if(pid==4){
-		pcb[pid].TF_ptr->eip = (unsigned int) STDIN;
+		pcb[pid].TF_ptr->eip = (unsigned int)STDIN;
 	}
 	else if(pid==5){
-		pcb[pid].TF_ptr->eip = (unsigned int) STDOUT;
+		pcb[pid].TF_ptr->eip = (unsigned int)STDOUT;
 	}
-	else pcb[pid].TF_ptr->eip = (unsigned int) UserProc;
+	else pcb[pid].TF_ptr->eip = (unsigned int)UserProc;
 
 		//fillout trapframe of new proc
    	pcb[pid].TF_ptr->eflags = EF_DEFAULT_VALUE | EF_INTR;
