79da1d28cfcdde44f8881d9c81a309813907fe5b
kennedys <plan9os2000@gmail.com>
2015-04-21 21:35:18 -0700

    Update proc.c

--- phase6fix/proc.c
+++ phase6fix/proc.c
@@ -86,3 +86,175 @@ void Init(){
 
 	}//while
 }
+/*------------------------------Phase 6 -----------------------------------------------------*/
+void shell () {
+   int BAUD_RATE, divisor;         // for serial port
+   msg_t msg;                      // local message space
+   char login[101], password[101]; // login and password strings entered
+   int STDIN = 4, STDOUT = 5;      // PID's of these processes
+
+  // initialize terminal interface data structure (below)
+   MyBzero((char *) &terminal.TX_q,sizeof(q_t));
+   MyBzero((char *) &terminal.RX_q,sizeof(q_t));
+   MyBzero((char *) &terminal.echo_q,sizeof(q_t));//clear 3 queues: TX_q, RX_q, echo_q
+   terminal.TX_sem = SemGet(Q_SIZE);   //get a semaphore to set TX_sem, count Q_SIZE (char space to terminal video)
+   terminal.RX_sem = SemGet(0);   //get a semaphore to set RX_sem, count 0 (no char from terminal KB)
+   terminal.echo = 1;   //set echo to 1 (default is to echo back whatever typed from terminal)
+   terminal.TX_extra = 1;   //set TX_extra to 1 (an IRQ3 TXRDY event missed)
+
+
+    /*
+   // COM1-8_IOBASE: 0x3f8 0x2f8 0x3e8 0x2e8 0x2f0 0x3e0 0x2e0 0x260
+   // transmit speed 9600 bauds, clear IER, start TXRDY and RXRDY
+   // Data communication acronyms:
+   //    IIR Intr Indicator Reg
+   //    IER Intr Enable Reg
+   //    ETXRDY Enable Xmit Ready
+   //    ERXRDY Enable Recv Ready
+   //    MSR Modem Status Reg
+   //    MCR Modem Control Reg
+   //    LSR Line Status Reg
+   //    CFCR Char Format Control Reg
+   //    LSR_TSRE Line Status Reg, Xmit+Shift Regs Empty
+   */
+   // set baud rate 9600
+   BAUD_RATE = 9600;              // Mr. Baud invented this
+   divisor = 115200 / BAUD_RATE;  // time period of each baud
+   outportb(COM2_IOBASE+CFCR, CFCR_DLAB);          // CFCR_DLAB 0x80
+   outportb(COM2_IOBASE+BAUDLO, LOBYTE(divisor));
+   outportb(COM2_IOBASE+BAUDHI, HIBYTE(divisor));
+   // set CFCR: 7-E-1 (7 data bits, even parity, 1 stop bit)
+   outportb(COM2_IOBASE+CFCR, CFCR_PEVEN|CFCR_PENAB|CFCR_7BITS);
+   outportb(COM2_IOBASE+IER, 0);
+   // raise DTR, RTS of the serial port to start read/write
+   outportb(COM2_IOBASE+MCR, MCR_DTR|MCR_RTS|MCR_IENABLE);
+   IO_DELAY();
+   outportb(COM2_IOBASE+IER, IER_ERXRDY|IER_ETXRDY); // enable TX, RX events
+   IO_DELAY();	
+   
+   while(1){ //inifite loop: 
+   
+     while(1){ // loop a
+     
+      // prompt valid commands (send msg to STDOUT, receive reply)
+       MyStrCpy(msg.data, "whoami, bye \n\0");
+       msg.recipient = STDOUT;
+       MsgSnd(&msg);
+       MsgRcv(&msg);
+      
+       //  prompt for login (send msg to STDOUT, receive reply)
+       MyStrCpy(msg.data, "login: \0");
+       msg.recipient = STDOUT;
+       MsgSnd(&msg);
+       MsgRcv(&msg);
+       
+       //  get login entered (send msg to STDIN, receive reply)
+	msg.recipient = STDIN;
+	MsgSnd(&msg);
+	MsgRcv(&msg);
+	MyStrCpy(login, msg.data);
+	
+       //  prompt for password (same as above)
+       MyStrCpy(msg.data, "password: \0");
+       msg.recipient = STDOUT;
+       MsgSnd(&msg);
+       MsgRcv(&msg);
+       
+       //  get password entered (same as above)
+       msg.recipient = STDIN;
+       MsgSnd(&msg);
+       MsgRcv(&msg);
+       MyStrCpy(password, msg.data);
+       
+        
+       if(MyStrCmp(login, password)) break; // string-compare login and password; if same, break loop A
+       
+       else {  //(else) prompt "Invalid login!\n\0"
+       	MyStrCpy(msg.data, "Invalid login! \n\0");
+       	msg.recipient = STDOUT;
+       	MsgSnd(&msg);
+       	MsgRcv(&msg);
+       }
+       
+     } //end of loop a
+     while(1) {//loop B:
+     //prompt for entering command string
+     MyStrCpy(msg.data, "enter command: \0"); 
+     msg.recipient = STDOUT;
+     MsgSnd(&msg);
+     MsgRcv(&msg);
+    
+     //get command string entered
+     msg.recipient = STDIN;
+     MsgSnd(&msg);
+     MsgRcv(&msg);
+     //   if command string is empty, then continue (loop B)
+      if(MyStrLen(msg.data) == 0) continue;
+      else if(MyStrCmp(msg.data, "bye\0")) break; //if command string is "bye", then break (loop B)
+      else if (MyStrCmp(msg.data, "whoami\0")) { //if command string is "whoami"
+       //show login string,
+       MyStrCpy(msg.data,login);
+       msg.recipient = STDOUT;//not sure if this is needed here
+       MsgSnd(&msg);
+       MsgRcv(&msg);
+       
+       //and an additional "\n\0" (for aesthetics)
+       msg.recipient = STDOUT;//doubly sure
+       MyStrCpy(msg.data, "\n\0");
+       MsgSnd(&msg);
+       MsgRcv(&msg);
+       continue; // continue (loop B)
+      } //if command string is "whoami" (end)
+      else { //else other strings are entered in command string
+   	//show "Command not found!\n\0"
+   	MyStrCpy(msg.data, "Command not found!\n\0");
+   	msg.recipient = STDOUT;
+   	MsgSnd(&msg);
+   	MsgRcv(&msg);
+      }
+     }// if command string is empty block (end)
+   }//  repeat loop B
+} //  repeat infinite loop
+   
+
+
+void STDIN(){
+char *p, ch;
+msg_t msg;
+	while(1){
+		MsgRcv(&msg);
+		p=msg.data;
+		while(1){
+			SemWait(terminal.RX_sem);
+			ch = (char)DeQ(&terminal.RX_q);
+			if(ch=='\r') break;
+			*p++ = ch;
+		}
+		*p = '\0';
+		msg.recipient = msg.sender;
+		MsgSnd(&msg);
+	}
+
+}
+	
+void STDOUT(){
+char *p;
+msg_t msg;
+	while(1){
+		MsgRcv(&msg);
+		p = msg.data;
+		while(*p != '\0'){ //loop A until p points to null
+			SemWait(terminal.TX_sem);
+			EnQ( *p, &terminal.TX_q);
+			TipIRQ3();
+			if(*p=='\n'){
+				SemWait(terminal.TX_sem);
+				EnQ(*p, &terminal.TX_q);
+			}		
+		p++;
+		}
+	msg.recipient = msg.sender;
+	MsgSnd(&msg);
+	}
+	
+}
