7e17672165dbc497ebdd674eb2f195eb9d55a920
AdamStillman <adam.still@gmail.com>
2015-04-28 00:33:46 -0700

    Update isr.c

--- phase7/isr.c
+++ phase7/isr.c
@@ -170,10 +170,11 @@ void MsgSndISR(){
 	int msg_id;
 	source = (msg_t *)pcb[CRP].TF_ptr->ebx;
 	msg_id = source -> recipient;
-
+	
+	source->sender = CRP;
+	source->time_stamp = sys_time;
 	if( mbox[msg_id].wait_q.size == 0 ){
-		source->sender = CRP;
-		source->time_stamp = sys_time;
+
 		MsgEnQ(source, &mbox[msg_id].msg_q);
 	}
 	else{
