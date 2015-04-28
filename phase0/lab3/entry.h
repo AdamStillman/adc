4e0638b614539a4d19f3b23943f1b5f9fc81ee38
kennedys <plan9os2000@gmail.com>
2015-04-28 03:50:54 -0700

    chang-ed code

--- phase0/lab3/entry.h
+++
@@ -1,7 +0,0 @@
-#ifndef _ENTRY_H_
-#define _ENTRY_H_
-#define TIMER_INTR 0x32
-#ifndef ASSEMBLER //skip if ASSEMBLER defined(in assembly code)
-void TimerEntry();//defined in entry.S assembly wont take this
-#endif
-#endif
